d48701da27d3fc1f9b35840c5f2d3aa8ca7cd553
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-10-02 17:16:37 +0100

    v1.0

--- contact-page/contact-page_script.js
+++ contact-page/contact-page_script.js
@@ -2,14 +2,13 @@ let form = document.querySelector("form");
 let fields = [];
 
 form.addEventListener("submit", (event) => {
+	event.preventDefault();
+	fields.name = document.querySelector("input[name=name]").value;
+	fields.email = document.querySelector("input[name=email]").value;
+	fields.phone = document.querySelector("input[name=phone]").value;
+	fields.message = document.querySelector("textarea[name=message]").value;
+	fields.preferEmail = document.querySelector("input[value=pref-email]").checked;
+	fields.preferTel = document.querySelector("input[value=pref-tel]").checked;
 
-  event.preventDefault();
-  fields.name = document.querySelector("input[name=name]").value;
-  fields.email = document.querySelector("input[name=email]").value;
-  fields.phone = document.querySelector("input[name=phone]").value;
-  fields.message = document.querySelector("textarea[name=message]").value;
-  fields.preferEmail = document.querySelector("input[name=contact-email]").checked;
-  fields.preferTel = document.querySelector("input[name=contact-tel]").checked;
-  
-  console.log(fields);
-})
+	console.log(fields);
+});

979ee7a35daebaf4029b3e0704a443d4dac4f3ee
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-10-02 01:15:57 +0100

    added value checks

--- contact-page_script.js
+++ contact-page_script.js
@@ -1,5 +1,15 @@
 let form = document.querySelector("form");
+let fields = [];
 
 form.addEventListener("submit", (event) => {
+
   event.preventDefault();
-});
+  fields.name = document.querySelector("input[name=name]").value;
+  fields.email = document.querySelector("input[name=email]").value;
+  fields.phone = document.querySelector("input[name=phone]").value;
+  fields.message = document.querySelector("textarea[name=message]").value;
+  fields.preferEmail = document.querySelector("input[name=contact-email]").checked;
+  fields.preferTel = document.querySelector("input[name=contact-tel]").checked;
+  
+  console.log(fields);
+})
